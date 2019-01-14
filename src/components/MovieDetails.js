fabf3a54c5f976a1feff4141b1aeb7179f5babbf
Alfonso Cornejo <alfunkso@outlook.com>
2019-01-14 14:50:43 -0300

    feat: Reviews moved to their own component

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,21 +1,37 @@
 import React, {PureComponent} from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import {connect} from 'react-redux';
-import {lazyFetchAllDetails} from "../actions";
+import {lazyFetchMovie} from "../actions";
 
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Reviews from './Reviews';
 
 const debug = require('debug')('prueba-tecnica-mediastream:MovieDetails');
 
 const styles = theme => ({
+    container: {
+        paddingTop: theme.spacing.unit * 1,
+        paddingBottom: theme.spacing.unit * 1,
+        paddingLeft: theme.spacing.unit * 4,
+        paddingRight: theme.spacing.unit * 4,
+    },
     poster: {
         maxWidth: "100%",
         maxHeight: "600px",
     },
     posterContainer: {
         textAlign: "center",
+    },
+    paper: {
+        paddingTop: theme.spacing.unit * 4,
+        paddingBottom: theme.spacing.unit * 4,
+        paddingLeft: theme.spacing.unit * 2,
+        paddingRight: theme.spacing.unit * 2,
+    },
+    adult: {
+        color: "red",
     },
 });
 
@@ -26,20 +42,24 @@ const mapStateToProps = (state, ownProps) => {
     if ( movie != null ) {
         const apiImages = state.getIn(["status", "apiImages"]);
         return {
+            movieId: movieId,
             title: movie.get("title"),
             voteAverage: movie.get("vote_average"),
+            voteCount: movie.get("vote_count"),
             overview: movie.get("overview"),
             releaseDate: movie.get("release_date"),
-            reviews: movie.get("reviews"),
             posterUrl: apiImages.getPosterUrl(movie.get("poster_path")),
+            fullPosterUrl: apiImages.getImageUrl(movie.get("poster_path"), "original"),
         };
     } else {
-        return {};
+        return {
+            movieId: movieId,
+        };
     }
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    onMount: () => dispatch(lazyFetchAllDetails(Number(ownProps.match.params.movieId))),
+    onMount: () => dispatch(lazyFetchMovie(Number(ownProps.match.params.movieId))),
 });
 
 class MovieDetails extends PureComponent {
@@ -50,78 +70,68 @@ class MovieDetails extends PureComponent {
     render() {
         const {
             classes,
+            movieId,
             title,
             voteAverage,
+            voteCount,
             releaseDate,
             overview,
-            reviews,
-            posterUrl
+            posterUrl,
+            fullPosterUrl,
+            adult,
         } = this.props;
 
-        debug("this.props.match.params", this.props.match.params);
-
         return (
-            <div>
-                <Paper>
+            <div className={classes.container}>
+                <Paper className={classes.paper}>
                     <Grid container>
                         <Grid item xs={6} className={classes.posterContainer}>
-                            <img
-                                src={posterUrl}
-                                alt={title}
-                                className={classes.poster}
-                            />
+                            {
+                                posterUrl != null
+                                ? (
+                                    <a
+                                        href={fullPosterUrl}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img
+                                            src={posterUrl}
+                                            alt={title}
+                                            className={classes.poster}
+                                        />
+                                    </a>
+                                    )
+                                : (
+                                    <Typography variant="h4">
+                                        No poster available
+                                    </Typography>
+                                    )
+                            }
+
                         </Grid>
                         <Grid item xs={6}>
-                            <Typography variant="display2">
+                            <Typography variant="h2" gutterBottom>
                                 {title}
                             </Typography>
-                            <Typography variant="title">
-                                Score: {voteAverage}
+                            <Typography variant="title" gutterBottom>
+                                Score: {voteAverage} ({voteCount} votes)
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                                {overview}
                             </Typography>
                             <Typography variant="body2">
                                 Release date: {releaseDate}
                             </Typography>
-                            <Typography variant="body1">
-                                {overview}
-                            </Typography>
+                            {
+                                (adult === "true" || adult) &&
+                                <Typography variant="button" className={classes.adult}>
+                                    Adult movie
+                                </Typography>
+                            }
                         </Grid>
                     </Grid>
-
-                    {
-                        reviews != null &&
-                        <Grid container style={{padding: "0 2em"}}>
-                            <Grid item xs={12}>
-                                <Typography variant="display1" gutterBottom>
-                                    Reviews
-                                </Typography>
-                                {
-                                    reviews.isEmpty()
-                                    ? (
-                                        <Typography variant="subtitle1" gutterBottom>
-                                            No reviews
-                                        </Typography>
-                                    )
-                                    : (
-                                        <div>
-                                        {
-                                            reviews.map((review, index) => (
-                                                <div key={index} style={{ padding: "2em" }}>
-                                                    <Typography variant="h5">
-                                                        {review.get("author")}
-                                                    </Typography>
-                                                    <Typography variant="body1">
-                                                        {review.get("content")}
-                                                    </Typography>
-                                                </div>
-                                            ))
-                                        }
-                                        </div>
-                                    )
-                                }
-                            </Grid>
-                        </Grid>
-                    }
                 </Paper>
+                <Reviews movieId={movieId} />
             </div>
         );
     }
